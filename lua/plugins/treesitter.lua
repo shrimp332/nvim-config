b4242e983346dbef3355f2f43506cb5dac66395b
shrimp332 <101687942+shrimp332@users.noreply.github.com>
2024-02-02 02:35:37 +1100

    init

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+    "nvim-treesitter/nvim-treesitter", 
+    build = ":TSUpdate",
+    config = function()
+        -- Treesitter setup
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = { "c", "lua", "vim", "javascript", "html", "css", "ruby" },
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
+        }
+        )
+    end
+}
