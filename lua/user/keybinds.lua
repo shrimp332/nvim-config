014f5ec084e394ad236a3ad7fcbb48570e174075
shrimp332 <101687942+shrimp332@users.noreply.github.com>
2024-02-02 02:51:56 +1100

    move keybinds into own file

---
+++ lua/user/keybinds.lua
@@ -0,0 +1,9 @@
+-- Set Leader
+vim.g.mapleader = " "
+
+-- Sets tab spacing to be 4 spaces
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
