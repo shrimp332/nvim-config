2af56f3fe8c11d906d1211b0aa3b1dce92409c61
shrimp332 <101687942+shrimp332@users.noreply.github.com>
2024-02-02 03:02:07 +1100

    add yazi

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,10 @@
+return {
+    "DreamMaoMao/yazi.nvim",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim",
+    },
+    config = function() 
+        vim.keymap.set('n', '<leader>y', ':Yazi<CR>')
+    end
+}
