6ca6090092916a67628de8962b68028c6b60a45c
shrimp332 <101687942+shrimp332@users.noreply.github.com>
2024-02-02 14:39:36 +1100

    add user sets

--- init.lua
+++ init.lua
@@ -1,23 +1,18 @@
 -- Load lazy package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("user/keybinds")
+require("user/sets")
 
 require("lazy").setup("plugins")
-
-
-
-
-
-
---
+++ lua/user/sets.lua
@@ -0,0 +1,11 @@
+-- Sets tab spacing to be 4 spaces
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+-- Enable Clipboard support
+vim.cmd("set clipboard=unnamedplus")
+
+-- Add relative line numbers
+vim.cmd("set relativenumber")
