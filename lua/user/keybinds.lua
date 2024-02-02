9ae11020309ce4efbee6d221abf84e5a6b80bde7
shrimp332 <101687942+shrimp332@users.noreply.github.com>
2024-02-02 16:41:18 +1100

    remove duplicate set

--- lua/user/keybinds.lua
+++ lua/user/keybinds.lua
@@ -1,10 +1,3 @@
 -- Set Leader
 vim.g.mapleader = " "
 
--- Sets tab spacing to be 4 spaces
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-
-
