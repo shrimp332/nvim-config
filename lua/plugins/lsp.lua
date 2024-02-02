203462d023a94b5fcdde9c54364c2d2e8603ae85
shrimp332 <101687942+shrimp332@users.noreply.github.com>
2024-02-02 14:38:42 +1100

    add snippets/completetion

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,15 +9,33 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls" }
+                ensure_installed = { "lua_ls", "rust_analyzer", "tsserver" }
             })
         end
     },
     {
         "neovim/nvim-lspconfig",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+        },
         config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
+
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.rust_analyzer.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.tsserver.setup({
+                capabilities = capabilities,
+            })
+
+            vim.keymap.set('n', '<leader>lh', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', '<leader>ld', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', '<leader>lD', vim.lsp.buf.declaration, {})
+            vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, {})
         end
     }
 }
---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,20 @@
+return {
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+                -- lua
+				null_ls.builtins.formatting.stylua,
+                -- ruby
+				null_ls.builtins.diagnostics.rubocop,
+				null_ls.builtins.formatting.rubocop,
+                -- Javascript
+				null_ls.builtins.diagnostics.esling,
+				null_ls.builtins.formatting.prettier,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, {})
+	end,
+}
