a7896edfb7e61031c7325e440d6dd6f2ebbd3b4b
shrimp332 <101687942+shrimp332@users.noreply.github.com>
2024-02-02 14:40:52 +1100

    add rust TS

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         -- Treesitter setup
         require("nvim-treesitter.configs").setup({
-            ensure_installed = { "c", "lua", "vim", "javascript", "html", "css", "ruby" },
+            ensure_installed = { "c", "lua", "vim", "javascript", "html", "css", "ruby", "rust" },
             auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
