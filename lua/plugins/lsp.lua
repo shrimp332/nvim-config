0e7c31abf553afa0a83152b94ae99bf5a0dd9e3b
shrimp332 <101687942+shrimp332@users.noreply.github.com>
2024-02-17 05:33:19 +1100

    move none-ls into lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -38,4 +38,39 @@ return {
 			vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { desc = "LSP CA" })
 		end,
 	},
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+			null_ls.setup({
+				sources = {
+					-- lua
+					null_ls.builtins.formatting.stylua,
+					-- ruby
+					null_ls.builtins.diagnostics.rubocop,
+					null_ls.builtins.formatting.rubocop,
+					-- Javascript
+                    -- null_ls.builtins.diagnostics.esling,
+					null_ls.builtins.formatting.prettier,
+				},
+			--	on_attach = function(client, bufnr)
+			--		if client.supports_method("textDocument/formatting") then
+			--			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			--			vim.api.nvim_create_autocmd("BufWritePre", {
+			--				group = augroup,
+			--				buffer = bufnr,
+			--				callback = function()
+			--					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+			--					-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+			--					vim.lsp.buf.format({ async = false })
+			--				end,
+			--			})
+			--		end
+			--	end,
+			})
+
+			vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { desc = "LSP Format" })
+		end,
+	},
 }
