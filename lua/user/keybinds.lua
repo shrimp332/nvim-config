413ba6418f8bc961092b4338390b70170497de63
shrimp332 <101687942+shrimp332@users.noreply.github.com>
2024-03-10 21:33:17 +1100

    update user

--- lua/user/keybinds.lua
+++ lua/user/keybinds.lua
@@ -1,10 +1,30 @@
+local map = vim.keymap.set
+
 -- Set Leader
 vim.g.mapleader = " "
 
 -- Splits
-vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Create a verticle split" })
-vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Create a horizontal split" })
-vim.cmd("nnoremap <C-j> <C-w>j")
-vim.cmd("nnoremap <C-k> <C-w>k")
-vim.cmd("nnoremap <C-l> <C-w>l")
-vim.cmd("nnoremap <C-h> <C-w>h")
+map("n", "<leader>sv", ":vsplit<CR>", { desc = "Create a verticle split" })
+map("n", "<leader>sh", ":split<CR>", { desc = "Create a horizontal split" })
+map("n", "<leader>sq", ":q<CR>", { desc = "Quit buffer" })
+
+map("n", "<C-j>", "<C-w>j", { desc = "Move focus to lower buffer" })
+map("n", "<C-k>", "<C-w>k", { desc = "Move focus to upper buffer" })
+map("n", "<C-l>", "<C-w>l", { desc = "Move focus to right buffer" })
+map("n", "<C-h>", "<C-w>h", { desc = "Move focus to left buffer" })
+
+map("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height" })
+map("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
+map("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
+map("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
+
+-- move lines
+map("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+map("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+map("i", "<A-j>", "<ESC>:m .+1<CR>==gi", { desc = "Move line down" })
+map("i", "<A-k>", "<ESC>:m .-2<CR>==gi", { desc = "Move line up" })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+
+-- Other
+map("n", "<C-s>", ":w<CR>", { desc = "Save buffer" })
---
+++ lua/user/opts.lua
@@ -0,0 +1,21 @@
+local opt = vim.opt
+
+-- Sets tab spacing to be 4 spaces
+opt.expandtab = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+
+-- Enable Clipboard support
+opt.clipboard = "unnamedplus"
+
+-- Add relative line numbers
+opt.number = true
+opt.relativenumber = true
+
+-- Splits right & below
+opt.splitbelow = true
+opt.splitright = true
+
+-- Set soft line limit
+-- opt.colorcolumn = 80
