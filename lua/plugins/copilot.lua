1cf25ffb32a0273ebd05c9c57da7b517d374ac5a
shrimp332 <101687942+shrimp332@users.noreply.github.com>
2024-12-04 17:58:59 +1100

    disable copilot default

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,1 +1,16 @@
-return { "github/copilot.vim" }
+return {
+  "github/copilot.vim",
+  init = function()
+    vim.g.copilot_enabled = 0
+
+    local toggle = function()
+      if vim.g.copilot_enabled == 0 then
+        vim.g.copilot_enabled = 1
+      else
+        vim.g.copilot_enabled = 0
+      end
+    end
+
+    vim.keymap.set("n", "<leader>cp", toggle, { desc = "Toggle Copilot" })
+  end
+}
