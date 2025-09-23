b3b6fb6e365d103a9b4b350e5f7be37542409b6d
Luke McDonald <101687942+shrimp332@users.noreply.github.com>
2025-09-23 12:18:28 +1000

    ts ignore php

--- lua/plugins/ts.lua
+++ lua/plugins/ts.lua
@@ -6,7 +6,7 @@ return {
             auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
-            ignore_install = { "latex" },
+            ignore_install = { "php" },
         })
     end
 }
