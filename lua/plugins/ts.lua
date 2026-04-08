7d74929cf8f3b850a87919dbf1f708ac2da43407
Luke McDonald <101687942+shrimp332@users.noreply.github.com>
2026-04-09 02:57:14 +1000

    update treesitter for nvim 0.12

--- lua/plugins/ts.lua
+++ lua/plugins/ts.lua
@@ -1,12 +1,15 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-            ignore_install = { "php" },
+    init = function()
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function()
+                -- Enable treesitter highlighting and disable regex syntax
+                pcall(vim.treesitter.start)
+                -- Enable treesitter-based indentation
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
         })
-    end
+    end,
 }
