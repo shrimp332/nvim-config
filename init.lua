7750df4ee7de48f4f8b4c4aea78b5247f16e1129
Luke McDonald <101687942+shrimp332@users.noreply.github.com>
2026-05-31 20:39:39 +1000

    disable copilot

--- init.lua
+++ init.lua
@@ -28,47 +28,47 @@ vim.diagnostic.config({ virtual_text = false })
 
 -- clipboard + over ssh
 vim.g.clipboard = {
-	name = "OSC 52",
-	copy = {
-		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-	},
-	paste = {
-		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-	},
+    name = "OSC 52",
+    copy = {
+        ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+        ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
 }
 opt.clipboard = "unnamedplus"
 
 local gh = function(x)
-	return "https://github.com/" .. x
+    return "https://github.com/" .. x
 end
 
 -- color scheme
 vim.pack.add({
-	gh("folke/tokyonight.nvim"),
+    gh "folke/tokyonight.nvim",
 })
 require("tokyonight").setup({
-	style = "night",
-	transparent = true,
+    style = "night",
+    transparent = true,
 })
 vim.cmd.colorscheme("tokyonight")
 
 -- navigation
 vim.pack.add({
-	gh("stevearc/oil.nvim"),
-	gh("nvim-tree/nvim-web-devicons"),
+    gh "stevearc/oil.nvim",
+    gh "nvim-tree/nvim-web-devicons",
 })
 
 require("oil").setup({
-	view_options = { show_hidden = true },
+    view_options = { show_hidden = true },
 })
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 vim.pack.add({
-	gh("nvim-telescope/telescope.nvim"),
-	gh("nvim-lua/plenary.nvim"),
-	gh("nvim-telescope/telescope-ui-select.nvim"),
+    gh "nvim-telescope/telescope.nvim",
+    gh "nvim-lua/plenary.nvim",
+    gh "nvim-telescope/telescope-ui-select.nvim",
 })
 require("telescope").setup({})
 require("telescope").load_extension("ui-select")
@@ -79,37 +79,37 @@ vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" 
 
 -- lsp, completions
 vim.pack.add({
-	{
-		src = gh("saghen/blink.cmp"),
-		version = "v1",
-	},
-	gh("rafamadriz/friendly-snippets"),
-	gh("folke/trouble.nvim"),
-	gh("mason-org/mason.nvim"),
-	gh("mason-org/mason-lspconfig.nvim"),
-	gh("neovim/nvim-lspconfig"),
+    {
+        src = gh "saghen/blink.cmp",
+        version = "v1",
+    },
+    gh "rafamadriz/friendly-snippets",
+    gh "folke/trouble.nvim",
+    gh "mason-org/mason.nvim",
+    gh "mason-org/mason-lspconfig.nvim",
+    gh "neovim/nvim-lspconfig",
 })
 require("blink.cmp").setup({
-	keymap = { preset = "default" },
+    keymap = { preset = "default" },
 
-	appearance = {
-		nerd_font_variant = "mono",
-	},
+    appearance = {
+        nerd_font_variant = "mono",
+    },
 
-	completion = { documentation = { auto_show = true } },
+    completion = { documentation = { auto_show = true } },
 
-	sources = {
-		default = { "lsp", "path", "snippets", "buffer" },
-	},
+    sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+    },
 
-	fuzzy = { implementation = "prefer_rust_with_warning" },
+    fuzzy = { implementation = "prefer_rust_with_warning" },
 
-	signature = {
-		enabled = true,
-		window = {
-			show_documentation = true,
-		},
-	},
+    signature = {
+        enabled = true,
+        window = {
+            show_documentation = true,
+        },
+    },
 })
 require("mason").setup()
 require("mason-lspconfig").setup()
@@ -123,80 +123,91 @@ vim.opt.signcolumn = "yes"
 vim.keymap.set({ "n", "x" }, "<leader>lf", "<cmd>lua vim.lsp.buf.format({async = true})<cr>")
 vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
 vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
+vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>")
 vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>")
 vim.keymap.set({ "n", "x" }, "<leader>lf", "<cmd>lua vim.lsp.buf.format({async = true})<cr>")
 vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>")
 
 -- server configs
 vim.lsp.config("emmet_language_server", {
-	filetypes = {
-		"astro",
-		"css",
-		"eruby",
-		"html",
-		"htmlangular",
-		"htmldjango",
-		"javascriptreact",
-		"less",
-		"pug",
-		"sass",
-		"scss",
-		"svelte",
-		"templ",
-		"typescriptreact",
-		"vue",
-		"php",
-	},
+    filetypes = {
+        "astro",
+        "css",
+        "eruby",
+        "html",
+        "htmlangular",
+        "htmldjango",
+        "javascriptreact",
+        "less",
+        "pug",
+        "sass",
+        "scss",
+        "svelte",
+        "templ",
+        "typescriptreact",
+        "vue",
+        "php",
+    },
 })
 vim.lsp.config("pylsp", {
-	settings = {
-		pylsp = {
-			plugins = {
-				pycodestyle = {
-					maxLineLength = 999,
-				},
-				mccabe = {
-					enabled = false,
-				},
-				pyflakes = { enabled = false },
-			},
-		},
-	},
+    settings = {
+        pylsp = {
+            plugins = {
+                pycodestyle = {
+                    maxLineLength = 999,
+                },
+                mccabe = {
+                    enabled = false,
+                },
+                pyflakes = { enabled = false },
+            },
+        },
+    },
 })
 vim.lsp.config("lua_ls", {
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
-			},
-		},
-	},
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" },
+            },
+        },
+    },
 })
 
+
 -- treesitter
-vim.pack.add({ gh("nvim-treesitter/nvim-treesitter") })
+vim.pack.add({ gh "nvim-treesitter/nvim-treesitter" })
 vim.api.nvim_create_autocmd("FileType", {
-	callback = function()
-		pcall(vim.treesitter.start)
-		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-	end,
+    callback = function()
+        pcall(vim.treesitter.start)
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
 })
 
 -- sugar
 vim.pack.add({
-	gh("lukas-reineke/indent-blankline.nvim"),
-	gh("windwp/nvim-autopairs"),
+    gh "lukas-reineke/indent-blankline.nvim",
+    gh "windwp/nvim-autopairs",
 })
 require("ibl").setup({ scope = { enabled = false } })
 require("nvim-autopairs").setup()
 
-vim.pack.add({ gh("github/copilot.vim") })
+-- vim.pack.add({ gh "github/copilot.vim" })
 
 -- latex
-vim.pack.add({ gh("lervag/vimtex") })
+vim.pack.add({ gh "lervag/vimtex" })
 vim.g.vimtex_view_method = "zathura"
 vim.g.vimtex_compiler_latexmk = {
-	aux_dir = "tmp",
-	out_dir = "tmp",
+    aux_dir = "tmp",
+    out_dir = "tmp",
 }
 vim.g.vimtex_compiler_latexmk_engines = { _ = "-xelatex" }
+
+-- Notes
+vim.opt.spelllang = "en_au"
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "markdown", "tex" },
+    callback = function()
+        vim.opt_local.spell = true
+    end
+})
